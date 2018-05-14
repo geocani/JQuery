8c1ecec093e96589f69d201f144c8e303fbe9424
Canivet Geoffrey <35733059+geocani@users.noreply.github.com>
2018-05-14 19:19:28 +0200

    nav ajax

---
+++ drill/nav ajax/ajax.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>slide</title>
+</head>
+<body>
+    <section class="page">
+        <div id="menu">
+            <ul>
+                <li><a href="#">un</a></li>
+                <li><a href="#">deux</a></li>
+                <li><a href="#">trois</a></li>
+                <li><a href="#">quarte</a></li>
+            </ul>
+        </div>
+        <div id="article">
+
+        </div>
+    </section>
+</body>
+</html>
